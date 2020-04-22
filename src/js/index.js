260618153e28cad5092362d17904564a693f5576
Iron318i <iron318i@gmail.com>
2020-04-22 16:13:19 +0300

    16-10

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,2 @@
 +function ($) {
-
-    $('body').append('<div id="toTop" class="btn">&nbsp</div>');
-    $(window).scroll(function () {
-	if ($(this).scrollTop() != 0) {
-	    $('#toTop').fadeIn();
-	} else {
-	    $('#toTop').fadeOut();
-	}
-    });
-
-
 }(jQuery);
